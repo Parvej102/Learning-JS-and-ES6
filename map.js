b3733f103f16dab9ae037d78f93afa7b494c43f4
MH Parvej <mhratan142@gmail.com>
2023-01-26 15:48:52 +0600

    added shortcut method

--- map.js
+++ map.js
@@ -3,3 +3,8 @@ const result = number.map(function(element){
     return element * element;
 })
 console.log(result);
+
+// shortcut method
+const number2 = [5, 6, 7, 9,];
+const result2 = number2.map( x=> x * x);
+console.log(result2);
