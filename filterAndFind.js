04f7c79091b8748dfafe288eb6f5a6b91e492c29
MH Parvej <mhratan142@gmail.com>
2023-01-26 16:08:36 +0600

    done

---
+++ filterAndFind.js
@@ -0,0 +1,9 @@
+
+const number = [2, 3, 4, 5, 6];
+
+const result = number.filter( x => x < 5);
+console.log(result);
+
+const number2 = [7, 8, 9, 10, 11];
+const result2 = number2.find ( x => x < 8);
+console.log(result2);
